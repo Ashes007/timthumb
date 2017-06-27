e1563ccde73f15af57d4439b47b027c18dd6f01b
Ashes007 <buieee2010@gmail.com>
2017-06-27 17:20:36 +0530

    My comment

---
+++ index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+<img src="http://localhost/st-aidans/TimThumb/timthumb.php?src=http://www.uniqueschoolapp.ie/images/album/1451/papers.co-mj83-leaf-bokeh-green-nature-36-3840x2400-4k-wallpaper.jpg&w=120&h=150&q=100" alt="img" />
+</body>
+</html>
